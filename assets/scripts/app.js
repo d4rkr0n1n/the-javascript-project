6e40da92e8c84944d267e9ba7c966381a3280fbd
Mridul Roy <midlry.mr@gmail.com>
2024-05-13 14:47:47 +0530

    update app.js and index.html

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,2 +1,7 @@
 // alert('This works!');
-let userName = 'Max';
+function add() {
+  num1 = document.getElementById('num1').value;
+  num2 = document.getElementById('num2').value;
+  sum = parseInt(num1) + parseInt(num2)
+    console.log(sum);
+}
